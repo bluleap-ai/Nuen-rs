bd10ce09b5df14b68ed501944fad8ecc12e95350
Tu Nguyen <tu.nguyen@ionmobility.com>
2024-10-11 15:29:45 +0700

    initalise for swgear input and bike output

---
+++ src/io.rs
@@ -0,0 +1,215 @@
+use defmt::info;
+use embassy_stm32::gpio::{Input, Output};
+
+pub struct SwitchGearInput {
+    pub kill_sw: Input<'static>,
+    pub mode_sw: Input<'static>,
+    pub side_stand_sw: Input<'static>,
+    pub reverse_sw: Input<'static>,
+    pub horn_sw: Input<'static>,
+    pub pha_cos_pw_sw: Input<'static>,
+    pub pha_cos_sw: Input<'static>,
+    pub left_braker_sw: Input<'static>,
+    pub right_braker_sw: Input<'static>,
+    pub keyfob_a_sw: Input<'static>,
+    pub keyfob_b_sw: Input<'static>,
+    pub keyfob_c_sw: Input<'static>,
+    pub keyfob_d_sw: Input<'static>,
+    pub turn_right_sw: Input<'static>,
+    pub turn_left_sw: Input<'static>,
+}
+
+pub struct BikeOutput {
+    pub seat_lock: Output<'static>,
+    pub tank_lock: Output<'static>,
+    pub sound_engine: Output<'static>,
+    pub braker_lamp: Output<'static>,
+    pub turn_right_lamp: Output<'static>,
+    pub pha_lamp: Output<'static>,
+    pub cos_lamp: Output<'static>,
+    pub license_lamp: Output<'static>,
+    pub horn: Output<'static>,
+    pub tail_lamp: Output<'static>,
+    pub turn_left_lamp: Output<'static>,
+}
+
+#[allow(dead_code)]
+impl BikeOutput {
+    pub fn set_all(&mut self, is_on: bool) {
+        if is_on {
+            self.seat_lock.set_high();
+            self.tank_lock.set_high();
+            self.sound_engine.set_high();
+            self.braker_lamp.set_high();
+            self.turn_right_lamp.set_high();
+            self.pha_lamp.set_high();
+            self.cos_lamp.set_high();
+            self.license_lamp.set_high();
+            self.horn.set_high();
+            self.tail_lamp.set_high();
+            self.turn_left_lamp.set_high();
+        } else {
+            self.seat_lock.set_low();
+            self.tank_lock.set_low();
+            self.sound_engine.set_low();
+            self.braker_lamp.set_low();
+            self.turn_right_lamp.set_low();
+            self.pha_lamp.set_low();
+            self.cos_lamp.set_low();
+            self.license_lamp.set_low();
+            self.horn.set_low();
+            self.tail_lamp.set_low();
+            self.turn_left_lamp.set_low();
+        }
+    }
+
+    pub fn seat_unlock(&mut self) {
+        self.seat_lock.set_high();
+    }
+
+    pub fn seat_lock(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn tank_lock(&mut self) {
+        self.tank_lock.set_high();
+    }
+
+    pub fn tank_unlock(&mut self) {
+        self.tank_lock.set_low();
+    }
+
+    pub fn sound_enable(&mut self) {
+        self.sound_engine.set_high();
+    }
+
+    pub fn braker_lamp_en(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn turn_right_lamp(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn pha_lamp(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn cos_lamp(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn license_lamp(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn horn_en(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn tail_lamp(&mut self) {
+        self.seat_lock.set_low();
+    }
+
+    pub fn turn_left_lamp(&mut self) {
+        self.seat_lock.set_low();
+    }
+}
+
+impl SwitchGearInput {
+    pub fn kill_sw(&self) -> bool {
+        self.kill_sw.is_high()
+    }
+
+    pub fn mode_sw(&self) -> bool {
+        self.mode_sw.is_high()
+    }
+
+    pub fn ss_sw(&self) -> bool {
+        self.side_stand_sw.is_high()
+    }
+
+    pub fn rev_sw(&self) -> bool {
+        self.reverse_sw.is_high()
+    }
+
+    pub fn horn_sw(&self) -> bool {
+        self.horn_sw.is_high()
+    }
+
+    pub fn pc_power_sw(&self) -> bool {
+        self.pha_cos_pw_sw.is_high()
+    }
+
+    pub fn pc_sw(&self) -> bool {
+        self.pha_cos_sw.is_high()
+    }
+
+    pub fn lb_sw(&self) -> bool {
+        self.left_braker_sw.is_high()
+    }
+
+    pub fn rb_sw(&self) -> bool {
+        self.right_braker_sw.is_high()
+    }
+
+    pub fn kf_a_sw(&self) -> bool {
+        self.keyfob_a_sw.is_high()
+    }
+
+    pub fn kf_b_sw(&self) -> bool {
+        self.keyfob_b_sw.is_high()
+    }
+
+    pub fn kf_c_sw(&self) -> bool {
+        self.keyfob_c_sw.is_high()
+    }
+
+    pub fn kf_d_sw(&self) -> bool {
+        self.keyfob_d_sw.is_high()
+    }
+
+    pub fn turn_r_sw(&self) -> bool {
+        self.turn_left_sw.is_high()
+    }
+
+    pub fn turn_l_sw(&self) -> bool {
+        self.turn_right_sw.is_high()
+    }
+
+    pub fn print_all(&self) {
+        info!(
+            "SW_GEAR_STATUS: \n
+                \tkill_sw: {}\n
+                \tmode_sw: {}\n
+                \tside_stand_sw: {}\n
+                \treverse_sw: {}\n
+                \thorn_sw: {}\n
+                \tpha_cos_power_sw: {}\n
+                \tpha_cos_sw: {}\n
+                \tleft_braker_sw: {}\n
+                \tright_braker_sw: {}\n
+                \tkeyfob_A_sw: {}\n
+                \tkeyfob_B_sw: {}\n
+                \tkeyfob_C_sw: {}\n
+                \tkeyfob_D_sw: {}\n
+                \tturn_right_sw: {}\n
+                \tturn_left_sw: {}\n",
+            self.kill_sw(),
+            self.mode_sw(),
+            self.ss_sw(),
+            self.rev_sw(),
+            self.horn_sw(),
+            self.pc_power_sw(),
+            self.pc_sw(),
+            self.lb_sw(),
+            self.rb_sw(),
+            self.kf_a_sw(),
+            self.kf_b_sw(),
+            self.kf_c_sw(),
+            self.kf_d_sw(),
+            self.turn_r_sw(),
+            self.turn_l_sw()
+        )
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -8,11 +8,15 @@ use embassy_stm32::can::{
     Can, CanRx, CanTx, Frame, Rx0InterruptHandler, Rx1InterruptHandler, SceInterruptHandler,
     StandardId, TxInterruptHandler,
 };
+use embassy_stm32::gpio::{Input, Level, Output, Pull, Speed};
 use embassy_stm32::peripherals::CAN1;
 use embassy_sync::{blocking_mutex::raw::NoopRawMutex, channel::Channel};
 use log::info;
 use static_cell::StaticCell;
 use {defmt_rtt as _, panic_probe as _};
+
+mod io;
+use io::{BikeOutput, SwitchGearInput};
 
 pub enum SimulinkType {
     KeyFob(u8),
@@ -41,15 +45,31 @@ bind_interrupts!(struct Irqs {
 static EXECUTOR: StaticCell<Executor> = StaticCell::new();
 
 #[embassy_executor::task]
-async fn state_machine_task(channel0: &'static ScreenBox, channel1: &'static SimulinkBox) {
-    match channel1.receive().await {
-        SimulinkType::KeyFob(state) => {
-            info!("Receive keyfob state {}", state);
+async fn state_machine_task(
+    sw_gear: SwitchGearInput,
+    mut bike_output: BikeOutput,
+    channel0: &'static ScreenBox,
+    channel1: &'static SimulinkBox,
+) {
+    loop {
+        // Check if receiving any data from other tasks.
+        if let Ok(data) = channel1.try_receive() {
+            match data {
+                SimulinkType::KeyFob(state) => {
+                    info!("Receive keyfob state {}", state);
+                }
+                SimulinkType::Can(frame) => {
+                    info!("Receive Can Frame {:?}", frame);
+                    channel0.send(ScreenRequest::LeftIndicator).await;
+                }
+            }
         }
-        SimulinkType::Can(frame) => {
-            info!("Receive Can Frame {:?}", frame);
-            channel0.send(ScreenRequest::LeftIndicator).await;
-        }
+
+        // Check SW gear input
+        sw_gear.print_all();
+
+        // Output
+        bike_output.set_all(false);
     }
 }
 
@@ -115,19 +135,63 @@ async fn keyfob_task(_channel: &'static SimulinkBox) {}
 #[embassy_executor::main]
 async fn main(spawner: Spawner) {
     let p = embassy_stm32::init(Default::default());
-    // let mut executor1: Executor = Executor::new();
+    let sw_input = SwitchGearInput {
+        kill_sw: Input::new(p.PA4, Pull::Up),
+        mode_sw: Input::new(p.PA6, Pull::Up),
+        side_stand_sw: Input::new(p.PA7, Pull::Up),
+        reverse_sw: Input::new(p.PA8, Pull::Up),
+        horn_sw: Input::new(p.PA9, Pull::Up),
+        pha_cos_pw_sw: Input::new(p.PA10, Pull::Up),
+        pha_cos_sw: Input::new(p.PA11, Pull::Up),
+        left_braker_sw: Input::new(p.PA12, Pull::Up),
+        right_braker_sw: Input::new(p.PB1, Pull::Up),
+        keyfob_a_sw: Input::new(p.PB12, Pull::Up),
+        keyfob_b_sw: Input::new(p.PB13, Pull::Up),
+        keyfob_c_sw: Input::new(p.PB10, Pull::Up),
+        keyfob_d_sw: Input::new(p.PC15, Pull::Up),
+        turn_right_sw: Input::new(p.PA15, Pull::Up),
+        turn_left_sw: Input::new(p.PB11, Pull::Up),
+    };
+
+    let bike_output = BikeOutput {
+        seat_lock: Output::new(p.PA2, Level::High, Speed::Low),
+        tank_lock: Output::new(p.PA3, Level::High, Speed::Low),
+        sound_engine: Output::new(p.PA5, Level::High, Speed::Low),
+        braker_lamp: Output::new(p.PB0, Level::High, Speed::Low),
+        turn_right_lamp: Output::new(p.PB3, Level::High, Speed::Low),
+        pha_lamp: Output::new(p.PB4, Level::High, Speed::Low),
+        cos_lamp: Output::new(p.PB5, Level::High, Speed::Low),
+        license_lamp: Output::new(p.PB14, Level::High, Speed::Low),
+        horn: Output::new(p.PB15, Level::High, Speed::Low),
+        tail_lamp: Output::new(p.PC13, Level::High, Speed::Low),
+        turn_left_lamp: Output::new(p.PC14, Level::High, Speed::Low),
+    };
+    // Init second executor.
     let executor = EXECUTOR.init(Executor::new());
+
+    // Init 2 embassy channels to transfer data.
     static CHANNEL0: StaticCell<SimulinkBox> = StaticCell::new();
-    let channel0 = &*CHANNEL0.init(Channel::new());
     static CHANNEL1: StaticCell<ScreenBox> = StaticCell::new();
+    let channel0 = &*CHANNEL0.init(Channel::new());
     let channel1 = &*CHANNEL1.init(Channel::new());
-    let mut can = Can::new(p.CAN1, p.PA11, p.PA12, Irqs);
+
+    // Initialize the CAN bus
+    let mut can = Can::new(p.CAN1, p.PB8, p.PB9, Irqs);
     can.modify_config().set_bitrate(500_000);
     can.enable().await;
     let (can_tx, can_rx) = can.split();
+
+    // spawn state machine task on main executor.
     spawner
-        .spawn(state_machine_task(channel1, channel0))
+        .spawn(state_machine_task(
+            sw_input,
+            bike_output,
+            channel1,
+            channel0,
+        ))
         .unwrap();
+
+    // spawn CAN and keyfob task on lower priority executor.
     executor.run(|spawn| {
         spawn.spawn(can_tx_task(can_tx, channel1)).unwrap();
         spawn.spawn(can_rx_task(can_rx, channel0)).unwrap();

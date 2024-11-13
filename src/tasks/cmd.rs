f667ef2748ad9eeffd131af769012051c3c395ea
Tu Nguyen <126753419+TuEmb@users.noreply.github.com>
2024-11-13 12:55:52 +0700

    add commandline module (#3)

--- src/tasks/cmd.rs
+++ src/tasks/cmd.rs
@@ -1,10 +1,11 @@
-use crate::{print, println};
+use crate::{cmd::CommandLine, print, println};
 use embassy_stm32::{mode::Async, usart::UartRx};
 use heapless::Vec;
 
 #[embassy_executor::task]
 pub async fn cmd_task(mut rx: UartRx<'static, Async>) {
     println!("hello CMD task!");
+    let mut cmd = init_command_line();
     let mut buffer = [0x00; 1];
     let mut vec_buffer: Vec<u8, 50> = Vec::new();
     while let Ok(_) = rx.read(&mut buffer).await {
@@ -16,8 +17,13 @@ pub async fn cmd_task(mut rx: UartRx<'static, Async>) {
         if buffer[0] == b'\n' || buffer[0] == b'\r' {
             print!("\n");
             if !vec_buffer.is_empty() {
-                let command_str = core::str::from_utf8(&vec_buffer[0..vec_buffer.len()]).unwrap();
-                process_command(command_str).await;
+                let command_str = core::str::from_utf8(&vec_buffer).unwrap();
+                if let Some(command) = cmd.check_command(command_str) {
+                    cmd.execute(command, command_str);
+                } else {
+                    println!("Invalid command");
+                    cmd.help_command();
+                }
             }
             print!("\x1b[32mnuen-embassy >\x1b[0m ");
             vec_buffer.clear(); // Clear the command string for the next command
@@ -35,29 +41,14 @@ pub async fn cmd_task(mut rx: UartRx<'static, Async>) {
     }
 }
 
-// Function to process the command string
-async fn process_command(command_str: &str) {
-    // Split command and arguments
-    let mut parts = command_str.split_whitespace();
-    if let Some(command) = parts.next() {
-        let args: Vec<&str, 10> = parts.collect();
-
-        // Match the command and handle accordingly
-        match command {
-            "help" => println!("Available commands: help, status, reset"),
-            "status" => println!("System status: OK"),
-            "reset" => println!("System resetting..."),
-            _ => println!(
-                "Unknown command '{}'. Type 'help' for a list of commands.",
-                command
-            ),
-        }
-
-        // Print out arguments for debugging
-        if !args.is_empty() {
-            println!("Arguments: {:?}", args);
-        }
-    } else {
-        println!("No command entered.");
-    }
+// init the command and callback function for it
+fn init_command_line() -> CommandLine<'static> {
+    let mut command_line = CommandLine::new();
+    command_line.add_command("hello", "Say hello", |_| println!("xin chao ban"));
+    command_line.add_command("bye", "Say goodbye", |_| println!("tam biet ban"));
+    command_line.add_command("reset", "reset the board", |_| {
+        println!("reset the board immediately !!!");
+        cortex_m::peripheral::SCB::sys_reset();
+    });
+    command_line
 }
